a2beab0737a9f0912585db4dc9a826935efaf1c1
Margarita <margarita27011991@gmail.com>
2022-08-28 16:34:29 +0300

    В двумерном массиве из целых чисел найти среднее арифметическое элементов в каждом столбце

--- task52/Program.cs
+++ task52/Program.cs
@@ -1,76 +1,24 @@
 ﻿/* Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
 Например, задан массив:
 1 4 7 2
-5 9 2 3
+5 9 2 4
 8 4 2 4
-Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3. */
-
-// Random rand = new Random();
-// Console.WriteLine("Введите m: ");
-// int m = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Введите n: ");
-// int n = Convert.ToInt32(Console.ReadLine());
-// double[,] matrix = new double [m, n];
-
-// for (int i = 0; i < m; i++)
-// {
-//     for (int j = 0; j < n; j++)
-//     {
-//         matrix[i, j] = new Random().Next(0, 10);
-//         Console.Write($"{matrix[i, j] + " "}");
-//     }
-//     Console.WriteLine();
-// }
-// double a = (matrix[0,0] + matrix[0,1] + matrix[0,2]) / 3;
-// double b = (matrix[1,0] + matrix[1,1] + matrix[1,2]) / 3;
-// double c = (matrix[2,0] + matrix[2,1] + matrix[2,2]) / 3;
-// Console.Write($"{a}, {b}, {c}");
-Console.WriteLine("введите количество строк");
-int n = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("введите количество столбцов");
-int m = Convert.ToInt32(Console.ReadLine());
-
-int[,] numbers = new int[n, m];
-FillArrayRandomNumbers(numbers);
-
-
-for (int j = 0; j < numbers.GetLength(1); j++)
+Среднее арифметическое каждого столбца: 4,6; 5,6; 4,6; 4. */
+const int M = 4;
+const int N = 4;
+double[,] matrix = new double [M, N];
+Random rand = new Random();
+for (int i = 0; i < M; i++)
 {
-    double avarage = 0;
-    for (int i = 0; i < numbers.GetLength(0); i++)
+    for (int j = 0; j < N; j++)
     {
-        avarage = (avarage + numbers[i, j]);
+        matrix[i, j] = new Random().Next(0, 10);
+        Console.Write($"{matrix[i, j] + " "}");
     }
-    avarage = avarage / n;
-    Console.Write(avarage + "; ");
+    Console.WriteLine();
 }
-Console.WriteLine();
-PrintArray(numbers);
-
-
-
-void FillArrayRandomNumbers(int[,] array)
-{
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            array[i, j] = new Random().Next(0, 10);
-        }
-    }
-}
-
-void PrintArray(int[,] array)
-{
-
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        Console.Write("[ ");
-        for (int j = 0; j < array.GetLength(1); j++)
-        {
-            Console.Write(array[i, j] + " ");
-        }
-        Console.Write("]");
-        Console.WriteLine("");
-    }
-}
+double a = Math.Round(((matrix[0,0] + matrix[0,1] + matrix[0,2] + matrix[0,3]) / 4), 2);
+double b = Math.Round(((matrix[1,0] + matrix[1,1] + matrix[1,2] + matrix[1,3]) / 4), 2);
+double c = Math.Round(((matrix[2,0] + matrix[2,1] + matrix[2,2] + matrix[2,3]) / 4), 2);
+double d = Math.Round(((matrix[2,0] + matrix[2,1] + matrix[2,2] + matrix[3,3]) / 4), 2);
+Console.Write($"{a}, {b}, {c}, {d}");
